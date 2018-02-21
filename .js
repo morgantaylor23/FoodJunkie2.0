68904b12a97fb41cf23ee52bcdbfb5b2aac2999d
morgantaylor23 <36573959+morgantaylor23@users.noreply.github.com>
2018-02-21 15:58:36 -0600

    Update .js

--- .js
+++ .js
@@ -1,4 +1,745 @@
-<!doctype html>
+var restaurants = [
+    {
+		"price": 1,
+		"name": "McDonalds",
+		"meals": ["breakfast", "lunch", "dinner"],
+		"type": ["fast food"]
+    },
+    {
+		"price": 1,
+		"name": "BurgerKing",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+    },
+    {
+		"price": 1,
+		"name": "Arbys",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "Wendys",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "Culvers",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "Sonic",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "Tacobell",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "Dairy Queen",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food", "ice cream"]
+	},
+	{
+		"price": 1,
+		"name": "Taco Johns",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "Tony's Tacos",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 1,
+		"name": "Subway",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "KFC",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "PizzaHut",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 1,
+		"name": "Culvers",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+		   
+	{
+		"price": 1,
+		"name": "Little Ceasars",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 1,
+		"name": "Dominos",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 1,
+		"name": "Caseys",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 1,
+		"name": "Marcos",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 1,
+		"name": "Uptown Pizza",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 1,
+		"name": "Papa Murphys",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 1,
+		"name": "First Wok",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 1,
+		"name": "Steak n' Shake",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 1,
+		"name": "Great Wall",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 1,
+		"name": "Chings",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 1,
+		"name": "China Star",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 1,
+		"name": "Fazolis",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 1,
+		"name": "Michaels",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 1,
+		"name": "La Gondola",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 1,
+		"name": "Eric's Resturaunt",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 1,
+		"name": "Fiesta Ranchera",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 1,
+		"name": "La Bamba",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 1,
+		"name": "Fiesta Ranchera",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 1,
+		"name": "Dunkin Donuts",
+		"meals": ["breakfast"],
+		"type": ["coffee"]
+	},
+	{
+		"price": 1,
+		"name": "Einsteins",
+		"meals": ["breakfast"],
+		"type": ["coffee"]
+	},
+	{
+		"price": 1,
+		"name": "Orange Leaf",
+		"meals": ["dessert"],
+		"type": ["dessert"]
+	},
+	{
+		"price": 1,
+		"name": "Carl's",
+		"meals": ["dessert"],
+		"type": ["dessert"]
+	},
+	{
+		"price": 1,
+		"name": "Emack & Bolio's",
+		"meals": ["dessert"],
+		"type": ["dessert"]
+	},
+	{
+		"price": 1,
+		"name": "Culvers",
+		"meals": ["dessert"],
+		"type": ["dessert"]
+	},
+	{
+		"price": 1,
+		"name": "Gene's",
+		"meals": ["dessert"],
+		"type": ["dessert"]
+	},
+	{
+		"price": 1,
+		"name": "Baskin Robins",
+		"meals": ["dessert"],
+		"type": ["dessert"]
+	},
+	{
+		"price": 1,
+		"name": "Merry Ann's Diner",
+		"meals": ["breakfast", "lunch", "dinner"],
+		"type": ["amercican"]
+	},
+	{
+		"price": 1,
+		"name": "Dennys",
+		"meals": ["breakfast"],
+		"type": ["american"]
+	},
+	{
+		"price": 1,
+		"name": "Bob Evans",
+		"meals": ["breakfast", "lunch", "dinner"],
+		"type": ["american"]
+	},
+	{
+		"price": 1,
+		"name": "IHOP",
+		"meals": ["breakfast"],
+		"type": ["american"]
+	},
+	{
+		"price": 3,
+		"name": "Anju Above",
+		"meals": ["lunch", "dinner"],
+		"type": ["boujee"]
+	},
+	{
+		"price": 3,
+		"name": "Tony Roma's",
+		"meals": ["lunch", "dinner"],
+		"type": ["american"]
+	},
+	{
+		"price": 3,
+		"name": "Biaggis",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 3,
+		"name": "Destihl",
+		"meals": ["lunch", "dinner"],
+		"type": ["boujee"]
+	},
+	{
+		"price": 2,
+		"name": "Olive Garden",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 3,
+		"name": "Medici",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 2,
+		"name": "Buffalo Wild Wings",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 3,
+		"name": "Pub II",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 3,
+		"name": "Maggie Mileys",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 3,
+		"name": "Dr McKay's",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 3,
+		"name": "Western Tap",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 3,
+		"name": "Mugsys",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 2,
+		"name": "Spotted Dog",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 2,
+		"name": "Legends",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 2,
+		"name": "Hooters",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 2,
+		"name": "Gill St. Bar and Grill",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 3,
+		"name": "Yumz",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 2,
+		"name": "Grand Cafe",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 3,
+		"name": "Imperial Buffet",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 2,
+		"name": "Lucky Garden",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 2,
+		"name": "Rosie's Pub",
+		"meals": ["lunch", "dinner"],
+		"type": ["bar"]
+	},
+	{
+		"price": 2,
+		"name": "Chick-fi-la",
+		"meals":["lunch", "dinner"],
+		"type":["fast food"]
+	},
+
+	{
+		"price": 2,
+		"name": "Popeyes",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 2,
+		"name": "Jimmy Johns",
+		"meals": ["lunch", "dinner"],
+		"type": ["fast food"]
+	},
+	{
+		"price": 2,
+		"name": "Monicals",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 2,
+		"name": "Tobins",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+
+	{
+		"price": 2,
+		"name": "Firehouse",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 2,
+		"name": "Blaze",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 2,
+		"name": "Pizza Ranch",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 2,
+		"name": "Flingers",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza", "bar"]
+	},
+	{
+		"price": 2,
+		"name": "Giordano's",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 2,
+		"name": "Joes Station House",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 2,
+		"name": "Rosati's",
+		"meals": ["lunch", "dinner"],
+		"type": ["pizza"]
+	},
+	{
+		"price": 2,
+		"name": "Puran",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 2,
+		"name": "Signature India",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 2,
+		"name": "Mandarin Garden",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 2,
+		"name": "Flat Top",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 2,
+		"name": "Golden Dragon",
+		"meals": ["lunch", "dinner"],
+		"type": ["asian"]
+	},
+	{
+		"price": 2,
+		"name": "Avantis",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 2,
+		"name": "Lucca Grill",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 2,
+		"name": "Cafe Italia",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 2,
+		"name": "DP Dough",
+		"meals": ["lunch", "dinner"],
+		"type": ["italian"]
+	},
+	{
+		"price": 2,
+		"name": "Moes",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 2,
+		"name": "Heredera",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 2,
+		"name": "Chipotle",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 2,
+		"name": "Los Potrillos",
+		"meals": ["lunch", "dinner"],
+		"type": ["mexican"]
+	},
+	{
+		"price": 2,
+		"name": "Fort Jesse Cafe",
+		"meals": ["breakfast", "lunch", "dinner"],
+		"type": ["american"]
+	},
+	{
+		"price": 2,
+		"name": "Coffee Hound",
+		"meals": ["breakfast"],
+		"type": ["coffee"]
+	},
+	{
+		"price": 2,
+		"name": "Coffee House",
+		"meals": ["breakfast"],
+		"type": ["coffee"]
+	},
+	{
+		"price": 2,
+		"name": "Starbucks",
+		"meals": ["breakfast"],
+		"type": ["coffee"]
+	},
+	{
+		"price": 2,
+		"name": "Gloria Jeans",
+		"meals": ["breakfast"],
+		"type": ["coffee"]
+	},
+	{
+		"price": 2,
+		"name": "Fusion Brew",
+		"meals": ["breakfast"],
+		"type": ["coffee"]
+	},
+	{
+		"price": 2,
+		"name": "Red Lobster",
+		"meals": ["lunch", "dinner"],
+		"type": ["american"]
+	},
+	{
+		"price": 2,
+		"name": "Applebees",
+		"meals": ["lunch", "dinner"],
+		"type": ["american"]
+	},
+	{
+		"price": 2,
+		"name": "Cold Stone",
+		"meals": ["lunch", "dinner"],
+		"type": ["dessert"]
+	},
+	{
+		"price": 2,
+		"name": "TGI Fridays",
+		"meals": ["lunch", "dinner"],
+		"type": ["americans"]
+	},
+	{
+		"price": 2,
+		"name": "Cracker Barrel",
+		"meals": ["breakfast","lunch", "dinner"],
+		"type": ["american"]
+	},
+	{
+		"price": 2,
+		"name": "Logan Roadhouse",
+		"meals": ["lunch", "dinner"],
+		"type": ["american"]
+	},
+	{
+		"price": 2,
+		"name": "Windy City Wieners",
+		"meals": ["lunch", "dinner"],
+		"type": ["americans"]
+	},
+	{
+		"price": 2,
+		"name": "Chilis",
+		"meals": ["lunch", "dinner"],
+		"type": ["american"]
+	},
+	{
+		"price": 2,
+		"name": "Insomnia",
+		"meals": ["dessert"],
+		"type": ["dessert"]
+	},
+	{
+		"price": 2,
+		"name": "Sugar Mama",
+		"meals": ["dessert"],
+		"type": ["dessert"]
+	}
+];
+
+// Looks like this:
+// {
+//      price: String,
+//      meal: String,
+//      cuisine: String
+// }
+var requests = {};
+
+$(document).ready(function() {
+    var viewport_height = $(window).height() - $("nav").outerHeight(true);
+    
+    var main = $("#main").height(viewport_height);
+    var chooser = $("#chooser").height(viewport_height);
+    var preference = $("#preference").height(viewport_height);
+    var cuisines = $("#cuisines").height(viewport_height);
+    var result = $("#result").height(viewport_height);
+    
+    requests.cuisine = [];
+    
+    main.show();
+    $("#hungrybutton").click(function() {
+        main.hide();
+        chooser.show();
+    });
+    $(".price").click(function() {
+        var price;
+        switch ($(this).html()) {
+            case "$":
+                price = 1;
+                break;
+            case "$$":
+                price = 2;
+                break;
+            case "$$$":
+                price = 3;
+                break;
+            default:
+                console.error("Something broke...");
+                break;
+        }
+        requests.price = price;
+        chooser.hide();
+        preference.show();
+    });
+    $(".meal").click(function() {
+        var meal;
+        switch ($(this).html()) {
+            case "Breakfast":
+                meal = "breakfast";
+                break;
+            case "Lunch":
+                meal = "lunch";
+                break;
+            case "Dinner":
+                meal = "dinner"
+                break;
+            case "Desert":
+                meal = "desert";
+                break;
+            default:
+                console.error("Something went horribly wrong...");
+        }
+        requests.meal = meal;
+        preference.hide();
+        cuisines.show();
+    });
+    
+    $('#cuisines input[type="checkbox"]').click(function() {
+        var string = $(this).data("ex");
+        requests.cuisine.push(string);
+    });
+    
+    $('#cuisines .submit').click(function() {
+        var res = get_resturaunt();
+        var name;
+        if (res != null) {
+            name = res.name;
+        } else {
+            name = "None found matching criteria"
+        }
+        $('#result-text').text(name);
+        cuisines.hide();
+        result.show();
+    });
+    console.log(restaurants.length);
+});
+
+function get_resturaunt() {
+	var hits = [];
+	
+	for(var i in restaurants){
+		var current_restaurant = restaurants[i];
+		if(current_restaurant.type.includes(requests.cuisine[0]) &&
+		(current_restaurant.meals.includes(requests.meal) &&
+		current_restaurant.price == requests.price)) {
+			hits.push(current_restaurant);
+        }
+	}
+	return hits[Math.floor(Math.random()*hits.length)];
+}<!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
